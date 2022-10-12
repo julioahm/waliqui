30387943f8e0cf7183ad1b3131e2def06ccc04a1
waliqi <115114878+waliqi@users.noreply.github.com>
2022-10-11 21:17:04 -0400

    sds

---
+++ cal/-/cmax.js
@@ -0,0 +1,31 @@
+function htmll()
+{
+document.writeln("<script>");
+document.writeln("    var playerInstance = jwplayer(\"player\");");
+document.writeln("    playerInstance.setup({");
+document.writeln("        playlist: [{");
+document.writeln("            \"title\": \"Reproductor | Diex TV\",");
+document.writeln("            \"description\": \"Mirar Cine MAX de manera gratuita\",");
+document.writeln("            \"image\": \"https://\",");
+document.writeln("            \"sources\": [");
+document.writeln("                {");
+document.writeln("                    \"default\": false,");
+document.writeln("                    \"type\": \"dash\",");
+document.writeln("                    \"file\": 'https://dtvott-cbc.akamaized.net/dash_live_1047/manifest.mpd',");
+document.writeln("                    \"drm\": {");
+document.writeln("                        \"clearkey\": { \"keyId\":  \"c3eeece1367c56f7a836c6d8ec1b016c\", \"key\": ");
+document.writeln("                        \"1277ade61ad4d3c6fede5c3f9da36b69\" }");
+document.writeln("                    },");
+document.writeln("                    \"label\": \"0\"");
+document.writeln("                }");
+document.writeln("            ]");
+document.writeln("        }],");
+document.writeln("        width: \"100%\",");
+document.writeln("        height: \"100%\",");
+document.writeln("        aspectratio: \"16:9\",");
+document.writeln("        autostart: true,");
+document.writeln("        cast: {},");
+document.writeln("        sharing: {}");
+document.writeln("    });");
+document.writeln("</script>");
+}
